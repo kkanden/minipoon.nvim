ebebcb57509df6f99cf3faca24cb7e9ba8cee009
oliwia <24637207+kkanden@users.noreply.github.com>
2025-09-11 23:28:56 +0200

    feat: basic add, select mark functionality

--- lua/minipoon/init.lua
+++ lua/minipoon/init.lua
@@ -4,8 +4,22 @@
 ---@alias Marks table<FileInRoot, MarkPos> Key-value table of files in project and their current mark position.
 ---@alias MarksList table<RootPath, Marks> Key-value table of RootPaths with all their existing marks.
 
+-- setup
+local menu_id = math.random(100000)
 local data_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "minipoon")
 local data_file = vim.fs.joinpath(data_dir, "marks.json")
+
+if vim.fn.isdirectory(data_dir) == 0 then
+	vim.fn.mkdir(data_dir, "p")
+	vim.fn.writefile({ "{}" }, data_file)
+elseif vim.fn.filereadable(data_file) == 0 then
+	vim.fn.writefile({ "{}" }, data_file)
+end
+
+local lines = vim.fn.readfile(data_file)
+lines = table.concat(lines) -- vim.json.decode needs a pure string
+---@type Marks
+local marks_list_global = vim.json.decode(lines)
 
 ---@as RootPath
 local root = vim.fs.root(0, ".git") or vim.uv.cwd()
@@ -24,74 +38,138 @@ if in_git then
 	end
 end
 
----@as FileInRoot
-local current_file = vim.fs.relpath(root, vim.fn.expand("%"))
-if not current_file then
-	error("minipoon: couldn't get current file path")
-	return
+-- functionality
+local function get_menu_name()
+	menu_id = menu_id + 1
+	return "__minipoon__" .. menu_id
 end
+local Marks = {}
 
-Marks = setmetatable({}, {})
-
-function Marks:new(tbl)
-	tbl = tbl or {}
-	setmetatable(tbl, self)
-	self.__index = self
-	return tbl
-end
-
-function Marks:add_mark()
-	local row, col = vim.api.nvim_win_get_position(0)
-	self[current_file] = { row = row, col = col }
-end
-
-function Marks:update_mark()
-	self:add_mark()
-end
-
-function Marks:get_display()
-	return vim.tbl_keys(self)
-end
-
----@as MarksList
-local MarksList = {}
-setmetatable(MarksList, { __index = {} })
-
----@param tbl Marks
-function MarksList:set_marks(tbl)
-	self[root] = tbl
-end
-
----@param root_path RootPath
-function MarksList:get_marks(root_path)
-	return self[root_path]
-end
-
-function MarksList:save_marks()
-	local json = vim.json.encode(self)
-
-	local ok, file = pcall(io.open, data_file, "w")
-	if not ok or not file then
-		error("minipoon: couldn't open data file: " .. file)
-		return
-	end
-
-	file:write(json)
-	file:close()
-end
-
-Window = {
+local window = {
 	buf = -1,
 	win = -1,
 }
 
-local function open_window(marks_list, buf, win_config)
-	win_config = win_config or {}
+function Marks:new()
+	if not marks_list_global[root_key] then
+		marks_list_global[root_key] = {}
+	end
+	local marks = setmetatable({
+		root = root,
+		win_config = {},
+		list = marks_list_global[root_key],
+	}, {
+		__index = Marks,
+	})
+	return marks
+end
+
+-- function Marks:_update_mark()
+-- 	self:add_mark()
+-- end
+
+function Marks:_get_display()
+	return vim.tbl_keys(self.list)
+end
+
+function Marks:_get_pos(mark_entry_name)
+	local pos = self.list[mark_entry_name]
+	return pos.row, pos.col
+end
+
+function Marks:_update_marks(marks_to_keep)
+	for k, _ in pairs(self.list) do
+		if not vim.tbl_contains(marks_to_keep, k) then
+			self.list[k] = nil
+		end
+	end
+end
+
+function Marks:_open(mark_entry_name)
+	self:toggle_window()
+
+	local file_path = mark_entry_name
+	if vim.fn.isabsolutepath(file_path) == 0 then
+		file_path = vim.fs.joinpath(self.root, file_path)
+	end
+	local buf = vim.fn.bufnr(file_path)
+	local should_set_cursor = false
+	if buf == -1 then
+		should_set_cursor = false
+		buf = vim.fn.bufadd(file_path)
+	end
+
+	if not vim.api.nvim_buf_is_loaded(buf) then
+		vim.fn.bufload(buf)
+	end
+
+	vim.api.nvim_set_current_buf(buf)
+
+	if should_set_cursor then
+		local pos = self:_get_pos(mark_entry_name)
+		vim.api.nvim_win_set_cursor(0, pos)
+	end
+end
+
+function Marks:add_mark()
+	local full_file_path = vim.fn.expand("%:p")
+	---@type FileInRoot
+	local current_file = vim.fs.relpath(root, full_file_path) or full_file_path
+	local row, col = vim.api.nvim_win_get_cursor(0)
+	self.list[current_file] = { row = row, col = col }
+end
+
+function Marks:close_window()
+	vim.api.nvim_win_hide(window.win)
+end
+
+function Marks:toggle_window()
+	local buf = window.buf
+	local win = window.win
+	local win_config = self.win_config or {}
+
+	if vim.api.nvim_win_is_valid(win) then
+		vim.api.nvim_win_hide(win)
+		return
+	end
+
 	if not vim.api.nvim_buf_is_valid(buf) then
 		buf = vim.api.nvim_create_buf(false, false)
 	end
+
 	vim.bo[buf].swapfile = false
 	vim.bo[buf].buflisted = false
+	vim.bo[buf].filetype = "minipoon"
+	vim.bo[buf].buftype = "acwrite"
+
+	if vim.api.nvim_buf_get_name(buf) == "" then
+		vim.api.nvim_buf_set_name(buf, get_menu_name())
+	end
+
+	vim.api.nvim_create_autocmd("BufWriteCmd", {
+		buffer = buf,
+		callback = function()
+			local entries = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+			self:_update_marks(entries)
+		end,
+	})
+
+	vim.api.nvim_create_autocmd("BufLeave", {
+		buffer = buf,
+		callback = function()
+			local entries = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+			self:_update_marks(entries)
+		end,
+	})
+
+	vim.keymap.set("n", "<CR>", function()
+		local entry = vim.api.nvim_get_current_line()
+		self:_open(entry)
+	end, { buffer = buf })
+
+	vim.keymap.set("n", "q", function()
+		self:close_window()
+	end, { buffer = buf })
 
 	---@type vim.api.keyset.win_config
 	local default_opts = {
@@ -105,26 +183,13 @@ local function open_window(marks_list, buf, win_config)
 		border = "rounded",
 	}
 
-	local contents = marks_list:get_display()
+	local contents = self:_get_display()
 	vim.api.nvim_buf_set_lines(buf, 0, -1, false, contents)
 
 	win_config = vim.tbl_deep_extend("force", win_config, default_opts)
 	local win = vim.api.nvim_open_win(buf, true, win_config)
-
-	Window = { buf = buf, win = win }
-end
-
-local function setup()
-	if vim.fn.isdirectory(data_dir) == 0 then
-		vim.fn.mkdir(data_dir, "p")
-		vim.fn.writefile({}, data_file)
-	end
-
-	vim.api.nvim_create_user_command("MiniPoon", function() end, {})
+	window = { buf = buf, win = win }
 end
 
 local marks = Marks:new()
-marks:add_mark()
-open_window(marks, Window.buf, {})
-
-setup()
+return marks
